a07a48450b74307fefdb785d07d320096b54cebf
NotAlxT <anhntran56@gmail.com>
2022-11-05 19:42:01 -0400

    Commit
    Commit

---
+++ Website/Java.js
@@ -0,0 +1,9 @@
+
+import Navbar from "navbar.jsx";
+
+reactDOM.render(
+    <div>
+        <h1>js test</h1>
+        <Navbar/>
+    </div>
+, document.querySelector("#app"));
---
+++ Website/js.components.jsx/navbar.jsx
@@ -0,0 +1,20 @@
+
+const Navbar = () => {
+    return (
+        <div>
+            <li>
+                <Link to="/">Dogs</Link>
+            </li>
+            <li>
+                <Link to="/cats">Cats</Link>
+            </li>
+            <li>
+                <Link to="/sheeps">Sheeps</Link>
+            </li>
+            <li>
+                <Link to="/goats">Goats</Link>
+            </li>
+        </div>
+    )
+}
+export default Navbar
